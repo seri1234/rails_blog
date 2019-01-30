79824d1248730409085abb60dbd367f02d259105
gitname <abc_x_y_z_12@yahoo.co.jp>
2019-01-30 05:57:04 +0000

    レイアウト要素の追加

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,22 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
-  test "should get root" do
-    get root_url
-    assert_response :success
-  end
-  
-  test "should get home" do
-    get static_pages_home_url
+#静的なページの単体テスト
+  test "should get home" do                                                     #root_path(/)にgetアクセスでページがきちんと表示され、
+    get root_path                                                               #titleもきちんとKohei's blogと表示されるか
     assert_response :success
     assert_select "title", "Kohei's blog"                                       #homeではkohei's blogとだけ表示
   end
   
-  test "should get about" do
-    get static_pages_about_url
+  test "should get about" do                                                    #about_path(/about)にgetアクセスでページがきちんと表示され、
+    get about_path                                                              #titleもきちんと表示されるか
     assert_response :success
     assert_select "title", "About | Kohei's blog"
   end
-
+  
+  test "should get contact" do                                                  #contact_path(/contact)にgetアクセスでページがきちんと表示され、
+    get contact_path                                                            #titleもきちんと表示されるか
+    assert_response :success
+    assert_select "title", "Contact | Kohei's blog"
+  end
 end
