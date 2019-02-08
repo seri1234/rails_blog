f2db5897d6decafaf1f016f53835585e5c51e8cb
gitname <abc_x_y_z_12@yahoo.co.jp>
2019-02-08 10:32:55 +0000

    improve-appearance

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -2,8 +2,9 @@ require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
   def setup
-    @comment = Comment.new(post_id: posts(:orange).id,
-                                     user_id: users(:michael).id)
+    @comment = Comment.new( post_id: posts(:orange).id,
+                            user_id: users(:michael).id,
+                            content: comments(:goodarticle).content)
   end
 
   test "should be valid" do                                                     #サニティーチェック
@@ -15,8 +16,13 @@ class CommentTest < ActiveSupport::TestCase
     assert_not @comment.valid?                                                  #valid?できちんとfalseになるか
   end
 
-  test "user_id should be present" do                                           ##post_idは存在しなければならない
+  test "user_id should be present" do                                           #post_idは存在しなければならない
     @comment.user_id = nil                                                      #post_idがnilの時
+    assert_not @comment.valid?                                                  #valid?できちんとfalseになるか
+  end
+  
+   test "content should be present" do                                          #contentは存在しなければならない
+    @comment.content = nil                                                      #contentがnilの時
     assert_not @comment.valid?                                                  #valid?できちんとfalseになるか
   end
 end
